065ae408020fc2756f4a7f3772cd8c94bbed402c
xiaotian <xiaotian@xingshulin.com>
2023-04-04 11:29:51 +0800

    chore: add docker-compose

--- scripts/init.sql
+++ scripts/init.sql
@@ -1,13 +1,13 @@
-CREATE DATABASE mylib;
+CREATE DATABASE if not exists mylib;
 
 USE mylib;
 
-CREATE TABLE `resource` (
-  `id` varchar(40) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `summary` text,
-  `type` enum('txt','pdf','word','excel') DEFAULT 'txt',
-  `created_time` int(11) DEFAULT NULL,
-  `updated_time` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE if not exists `resource` (
+    `id` varchar(40) NOT NULL,
+    `name` varchar(255) NOT NULL,
+    `summary` text,
+    `type` enum('txt','pdf','word','excel') DEFAULT 'txt',
+    `created_time` int(11) DEFAULT NULL,
+    `updated_time` int(11) DEFAULT NULL,
+    PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
